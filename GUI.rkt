35709fe1f877213c3e1789acbc74b8d9e1c4f4f0
Kearranza <cbkevin.ant@gmail.com>
2023-05-16 17:46:48 -0600

    Animación del Caballo Finalizada
    La parte gráfica estaría prácticamente lista a espera nada más de unos cuantos retoques finales y algunos comentados.

--- GUI.rkt
+++ GUI.rkt
@@ -3,8 +3,12 @@
 (require racket/draw)
 
 (define horse-shown #f)
+(define horse-i 0)
+(define horse-j 0)
+(define current-value 1)
+(define visited-cells '())
 
-(define (crear-tablero matriz)
+(define (crear-tablero-draw matriz)
   (define n (length matriz))
   (define tablero (make-bitmap (* n 50) (* n 50)))
   (define dc (new bitmap-dc% [bitmap tablero]))
@@ -13,25 +17,28 @@
       (define color (if (even? (+ i j)) "white" "black"))
       (send dc set-brush color 'solid)
       (send dc draw-rectangle (* i 50) (* j 50) 50 50)))
+  (for ([cell visited-cells])
+  (define i (first cell))
+  (define j (second cell))
+  (send dc set-pen "red" 5 'solid)
+  (send dc draw-point (+ (* i 50) 25) (+ (* j 50) 25)))
   (when horse-shown
     (define horse-text "♞")
     (send dc set-text-foreground "red")
     (send dc set-font (make-object font% 36 'default))
     (define-values [w h ascent descent] (send dc get-text-extent horse-text))
-    (define x-offset (- (/ 50 2) (/ w 2)))
-    (define y-offset (- (/ 50 2) (/ h 2)))
+    (define x-offset (+ (* horse-i 50) (- (/ 50 2) (/ w 2))))
+    (define y-offset (+ (* horse-j 50) (- (/ 50 2) (/ h 2))))
     (send dc draw-text horse-text x-offset y-offset))
   tablero)
 
 (define matriz-prueba
   (list
-   (list 1 2 3 4 5 6 7)
-   (list 4 5 6 4 5 6 7)
-   (list 4 5 6 4 5 6 7)
-   (list 4 5 6 4 5 6 7)
-   (list 4 5 6 4 5 6 7)
-   (list 4 5 6 4 5 6 7)
-   (list 4 5 6 4 5 6 7)))
+   (list 2 9 3 13)
+   (list 4 5 6 7)
+   (list 14 8 1 15)
+   (list 16 11 12 10)
+   ))
 
 (define tablero-prueba #f)
 
@@ -50,7 +57,7 @@
        [paint-callback
         (λ (_ dc)
           (set! tablero-prueba
-                (crear-tablero matriz-prueba))
+                (crear-tablero-draw matriz-prueba))
           (define tablero-width (* 50 (length matriz-prueba)))
           (define tablero-height (* 50 (length matriz-prueba)))
           (define x-offset (/ (- frame-width tablero-width) 2))
@@ -63,6 +70,11 @@
        [label "Colocar Caballo"]
        [callback
         (λ _
+          (for ([i (in-range (length matriz-prueba))])
+            (for ([j (in-range (length matriz-prueba))])
+              (when (= (list-ref (list-ref matriz-prueba i) j) 1)
+                (set! horse-i j)
+                (set! horse-j i))))
           (set! horse-shown #t)
           (send comenzar-button enable #t)
           (send colocar-caballo-button enable #f)
@@ -71,10 +83,19 @@
 (define comenzar-button
   (new button%
        [parent frame]
-       [label "Comenzar"]
-       [enabled #f]))
+       [label "Movimiento"]
+       [enabled #f]
+       [callback
+        (λ _
+          (set! current-value (+ current-value 1))
+          (set! visited-cells (cons (list horse-i horse-j) visited-cells))
+          (for ([i (in-range (length matriz-prueba))])
+            (for ([j (in-range (length matriz-prueba))])
+              (when (= (list-ref (list-ref matriz-prueba i) j) current-value)
+                (set! horse-i j)
+                (set! horse-j i))))
+          (send canvas refresh))]))
 
 (send frame show #t)
-
 
 
